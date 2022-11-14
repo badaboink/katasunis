302fcee8e9d51bf0b4b8568c1edadf8d46cdec96
Agnė Rauluševičiūtė <agne.rauluseviciute@ktu.edu>
2022-11-14 18:59:01 +0200

    produktai

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,30 @@
-import React from 'react';
-import '../../App.css';
+// import React from 'react';
+// import '../../App.css';
+// //className='products'
+// export default function Products() {
+//   return (
+//   <div>
+//   <h1>PRODUCTS</h1>
+//   <button>Pridėti naują prekę</button>
+//   </div>
+//   );
+// }
 
-export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>;
+import React from 'react';
+//import './App.css';
+import '../../App.css';
+import './products.css';
+import ProductList from '../ProductList';
+
+function Products() {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1>AUGINTINIŲ PREKĖS</h1>
+      </header>
+      <ProductList/>
+    </div>
+  );
 }
+
+export default Products;
