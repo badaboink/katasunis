52fa848f5f7648d74db8eb094f75e84b38307638
badaboink <eglejanusaityte@gmail.com>
2022-12-12 21:20:23 +0200

    Added: profile viewing and deleting (admin)

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,54 +1,66 @@
 import React from "react";
 import "../../App.css";
 import "./Table.scss"
+import { useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-export default function Services() {
-  return (
-    <React.Fragment>
-      <center>
-        <h1>VISI NAUDOTOJAI:</h1>
-      </center>
-      <center>
-  
-      <div class="container">
-  <ul class="responsive-table">
-    <li class="table-header">
-      <div class="col col-1">Vardas pavardė</div>
-      <div class="col col-2">Lytis</div>
-      <div class="col col-3">Gimimo data</div>
-      <div class="col col-4">el-paštas</div>
-      <div class="col col-5">telefono numeris</div>
-      <div class="col col-6"></div>
-      <div class="col col-7"></div>
-    </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Vardas pavardė">Vardenė Pavardenė</div>
-      <div class="col col-2" data-label="Lytis">M</div>
-      <div class="col col-3" data-label="Gimimo data">2022-12-11</div>
-      <div class="col col-4" data-label="el-paštas">email@gmail.com</div>
-      <div class="col col-5" data-label="telefono numeris">+37063666666</div>
-      <div class="col col-6" data-label="edit"><Link to="/redaguoti-naudotoja">Redaguoti</Link></div>
-      <div class="col col-7" data-label="delete"><Link to="/salinti-naudotoja">Šalinti</Link></div>
-    </li>
-    <li class="table-row">
-      <div class="col col-1" data-label="Vardas pavardė">Vardenė Pavardenė</div>
-      <div class="col col-2" data-label="Lytis">M</div>
-      <div class="col col-3" data-label="Gimimo data">2022-12-11</div>
-      <div class="col col-4" data-label="el-paštas">email@gmail.com</div>
-      <div class="col col-5" data-label="telefono numeris">+37063666666</div>
-      <div class="col col-6" data-label="edit"><Link to="/redaguoti-naudotoja">Redaguoti</Link></div>
-      <div class="col col-7" data-label="delete"><Link to="/salinti-naudotoja">Šalinti</Link></div>
-    </li>
-    
-  </ul>
-</div>
-      </center>
-      <center>
-        <div>
-          <Link to="/kurti-naudotoja">Kurti naudotoją</Link>
-        </div>
-      </center>
-    </React.Fragment>
-  );
+
+export default function ViewProfiles()
+{
+    const [users, setUsers] = useState([]);
+    const [id, setId] = useState(null);
+    const history = useHistory();
+    useEffect(() => {
+      getUsers();
+    }, []);
+    const getUsers = () => {
+      fetch('http://localhost/katasunis/katasunis_backend/perziureti_naudotojus.php')
+          .then((response) => response.json())
+          .then((data) => setUsers(data));
+    };
+    const DeleteProfile = (event, id) => {
+      // Only make the DELETE request when the "Šalinti" text is clicked
+      if (event.target.innerHTML === "Šalinti") {
+        // Send a DELETE request to the server with the user's ID
+        fetch(`http://localhost/katasunis/katasunis_backend/istrinti_naudotojus.php?id=${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then((response) => {
+          console.log(response);
+          // You may want to update the list of users here by making another
+          // request to the server to get the updated list of users
+          window.location.reload();
+        });
+      }
+      event.preventDefault();
+    };
+    return (
+      <ul class="responsive-table">
+           {
+            users.map((user) => (
+              
+              <li class="table-row">
+              <div class="col col-1">{user.vardas}</div>
+              <div class="col col-2">{user.pavarde}</div>
+              <div class="col col-3">{user.el_pastas}</div>
+              <div class="col col-4">{user.lygis}</div>
+              <div class="col col-4">{user.lytis}</div>
+              <div class="col col-4">{user.telefono_numeris}</div>
+              <div class="col col-4"> 
+              <Link
+              to={{ pathname: `/Redaguoti-naudotoja/${user.id}`, state: id }}
+              className={`card-wrapper restore-${id}`}
+            >
+              Redaguoti
+            </Link></div>
+              <div class="col col-4" onClick={(event) => DeleteProfile(event, user.id)} data-label="Salinti"><button>Šalinti</button></div>
+              </li>
+            ))
+            }
+      </ul>
+    );
 }
