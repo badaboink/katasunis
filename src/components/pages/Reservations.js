0bf5b803cba0a0bb7db7799b813eb1339c095bcb
Kornelija <kornelija.magylaite@gmail.com>
2022-12-12 19:57:09 +0200

    rezervacija

--- src/components/pages/Reservations.js
+++ src/components/pages/Reservations.js
@@ -1,70 +1,65 @@
 //components/ProductList.js
 
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import "./Table.scss";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-class Reservations extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //   }
+export default function Reservations() {
+  const [reservations, setReservations] = useState([]);
 
-  render() {
-    return (
-      <Fragment>
-        <center>
-          <h1>VISOS REZERVACIJOS:</h1>
-        </center>
-        <center>
-          <div>
+  useEffect(() => {
+    getReservations();
 
-            <div class="container">
-              <ul class="responsive-table">
-                <li class="table-header">
-                  <div class="col col-1">Klientas</div>   
-                  <div class="col col-2">Data</div>
-                  <div class="col col-3">Būsena</div>
+  }, []);
 
-                </li>
-                <li class="table-row">
-                  <div class="col col-1" data-label="Klientas">Kornelija Magylaitė</div>
-                  <div class="col col-2" data-label="Data">2022-11-15 11:00</div>
-                  <div class="col col-3" data-label="Būsena">Patvirtinta</div>
+  const getReservations = () => {
+    fetch("http://localhost/katasunis/katasunis_backend/selectRezervacijos.php")
+      .then((response) => response.json())
+      .then((data) => {
+        //console.log(data);
+        setReservations(data);
+      });
+  };
 
-                </li>
-                <li class="table-row">
-                  <div class="col col-1" data-label="Klientas">Eglė Janušaitytė</div>
-                  <div class="col col-2" data-label="Data">2022-11-25 15:00</div>
-                  <div class="col col-3" data-label="Būsena">Nepatvirtinta</div>
+  
 
-                </li>
-                <li class="table-row">
-                  <div class="col col-1" data-label="Klientas">Agnė Rauluševičiūtė</div>
-                  <div class="col col-2" data-label="Data">2022-12-15 11:3</div>
-                  <div class="col col-3" data-label="Būsena">Patvirtinta</div>
-
-                </li>
-                <li class="table-row">
-                  <div class="col col-1" data-label="Klientas">Justas Lisajus</div>
-                  <div class="col col-2" data-label="Data">2022-11-16 18:00</div>
-                  <div class="col col-3" data-label="Būsena">Patvirtinta</div>
-                </li>
-                <li class="table-row">
-                  <div class="col col-1" data-label="Klientas">Vytas Paliukas</div>
-                  <div class="col col-2" data-label="Data">2022-11-20 17:00</div>
-                  <div class="col col-3" data-label="Būsena">Nepatvirtinta</div>
-                </li>
-
-              </ul>
-            </div>
-            <Link to="/kurti-rezervacija">
-              Kurti rezervaciją
-            </Link>
+  return (
+    <Fragment>
+      <center>
+        <h1>VISOS REZERVACIJOS:</h1>
+      </center>
+      <center>
+        <div>
+          <div class="container">
+            <ul class="responsive-table">
+              <li class="table-header">
+                <div class="col col-1">Klientas</div>
+                <div class="col col-2">Data</div>
+                <div class="col col-3">Laikas</div>
+                <div class="col col-4">Būsena</div>
+              </li>
+              {reservations.map((reservation, index) => (
+                  <li class="table-row" key={index}>
+                    <div class="col col-1" data-label="Klientas">
+                    {reservation.vardas + " " + reservation.pavarde}
+                    </div>
+                    <div class="col col-2" data-label="Data">
+                     {reservation.data}
+                    </div>
+                    <div class="col col-3" data-label="Laikas">
+                     {reservation.laikas}
+                    </div>
+                    <div class="col col-4" data-label="Būsena">
+                      
+                    </div>
+                  </li>
+                      ))}
+            </ul>
           </div>
-        </center>
-      </Fragment>
-    );
-  }
+          <Link to="/kurti-rezervacija">Kurti rezervaciją</Link>
+        </div>
+      </center>
+    </Fragment>
+  );
 }
-
-export default Reservations;
